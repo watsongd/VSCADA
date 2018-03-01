1a8c26bec87b0303ee461579bbacbae026da26dd
Jefe <watsongd@lafayette.edu>
2018-03-01 18:46:38 +0000

    Commented print config list

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ with open('config.csv', newline='') as csvfile:
         sensor = SensorInfo(name=row[0], lower_threshold=int(row[1]), upper_threshold=int(row[2]), drop_out=int(row[3]))
         sensor_thresh_list.append(sensor)
 
-print (sensor_thresh_list)
+#print (sensor_thresh_list)
