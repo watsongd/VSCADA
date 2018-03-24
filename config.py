0393c73f699fb4c5b4fe680782fdc81c4dedfce5
Jefe <watsongd@lafayette.edu>
2018-03-24 21:02:48 +0000

    Chen changes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ sensor_thresh_list = []
 
 SensorInfo = collections.namedtuple('sensor', 'name lower_threshold upper_threshold drop_out')
 
-with open('config.csv', newline='') as csvfile:
+with open('/home/pi/Desktop/VSCADA/config.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
         sensor = SensorInfo(name=row[0], lower_threshold=int(row[1]), upper_threshold=int(row[2]), drop_out=int(row[3]))
