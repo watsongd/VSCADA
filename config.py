fef90410b6d20901086a6e6f0adc76a1263231ad
connor <nacec@lafayette.edu>
2018-03-24 17:09:44 -0400

    Testing config file locations

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 sensor_thresh_list = []
 
 SensorInfo = collections.namedtuple('sensor', 'name lower_threshold upper_threshold drop_out')
-
+'''
 config_path = ''
 
 for root, dirs, files in os.walk("/media/pi"):
@@ -18,7 +18,9 @@ for root, dirs, files in os.walk("/media/pi"):
             config_path ='/home/pi/Desktop/VSCADA/' + 'config.csv'
 
 print (config_path)
-with open(config_path, newline='') as csvfile:
+'''
+
+with open('/home/pi/Desktop/VSCADA/config.csv' , newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
         sensor = SensorInfo(name=row[0], lower_threshold=int(row[1]), upper_threshold=int(row[2]), drop_out=int(row[3]))
