269965722900722b531f1f728b22a27cdad6087c
connor <nacec@lafayette.edu>
2018-04-03 10:08:36 -0400

    Drop out changes

--- error.py
+++ error.py
@@ -1,5 +1,6 @@
 class Error(object):
 
-	def __init__(self, name, num_errors):
-		self.name = name
+	def __init__(self, sensor_id, num_errors, critical_error):
+		self.sensor_id = sensor_id
 		self.num_errors = num_errors
+		self.critical_error = critical_error
