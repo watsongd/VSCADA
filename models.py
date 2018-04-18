9671493115f4eb65214a57915f93e94fd054074d
Connor N <nacec@lafayette.email>
2018-04-17 20:15:33 -0400

    fixed an issue with export

--- models.py
+++ models.py
@@ -103,7 +103,7 @@ def export_csv_previous(session):
         g = open(flash_drive_path + '/car_data_recovery_session_{}.csv'.format(session), 'w')
 
         writer = csv.writer(g, delimiter=',')
-        writer.writerows(['Sensor_id', 'Time', 'Data Value', 'Flagged?'])
+        #writer.writerows(['Sensor_id', 'Time', 'Data Value', 'Flagged?'])
         writer.writerows(data_session)
 
         g.close()  
